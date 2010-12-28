ea0658df066117e8aa9a445c7080a03df9cfc6a1
Michael Mohr <akihana@gmail.com>
2010-12-28 10:57:24 -0800

    let gcc handle the IT blocks implicitly

--- Android.mk
+++ Android.mk
@@ -100,7 +100,7 @@ ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 #  -ftree-vectorizer-verbose=2: verbose output during compile
 # Note: not all V7a targets support NEON!
 #  LOCAL_ARM_NEON := true
-LOCAL_CFLAGS += -DTFM_ARM
+LOCAL_CFLAGS += -DTFM_ARM -mimplicit-it=thumb
 endif
 
 include $(BUILD_STATIC_LIBRARY)
