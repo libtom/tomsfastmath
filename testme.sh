87aa730c59a8974b444b874f68c42ecc35afe606
Steffen Jaeckel <s@jaeckel.eu>
2015-02-15 23:40:45 +0100

    testme: fix traps

--- testme.sh
+++ testme.sh
@@ -5,15 +5,15 @@
 _runtest()
 {
   echo -n "Run test $1 $2"
-  trap 'echo " - build not successful, errors are:" && cat test_gcc_errors.txt' INT TERM
+  trap 'echo " - build not successful, errors are:" && cat test_gcc_errors.txt' INT TERM EXIT
   make clean > /dev/null
   CC="${1}" make test_standalone -j9 $2 > /dev/null 2>test_gcc_errors.txt
-  trap - INT TERM
+  trap - INT TERM EXIT
   local outfile="test_$(echo ${1}${2} | tr -d '\"' | tr ' ' '_').txt"
-  trap 'echo " - tests not successful, failed at:" && tail ${outfile}' INT TERM
+  trap 'echo " - tests not successful, failed at:" && tail ${outfile}' INT TERM EXIT
   ./test > ${outfile}
   echo " successful"
-  trap - INT TERM
+  trap - INT TERM EXIT
 }
 
 gccopt="-m32 -m64 -mx32"
